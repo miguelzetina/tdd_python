ced92815a9c54651e762cc38c216b502687380ba
Miguel Angel Zetina Zetina <loefthur26@gmail.com>
2018-04-24 11:47:10 -0500

    Added update stock test

--- stock_alerter/tests/test_stock.py
+++ stock_alerter/tests/test_stock.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 import unittest
 
 from ..stock import Stock
@@ -7,3 +9,13 @@ class StockTest(unittest.TestCase):
     def test_price_of_a_new_stock_class_shoulb_be_None(self):
         stock = Stock("GOOG")
         self.assertIsNone(stock.price)
+
+    def test_stock_update(self):
+        """
+        An update should set the price on the stock object
+        We will be using the 'datetime' module for the timestamp
+        This test follows the pattern Arrange-Act-Assert
+        """
+        goog = Stock("GOOG")
+        goog.update(datetime(2014, 2, 12), price=10)
+        self.assertEqual(10, goog.price)
