2038de0503dfa46c333b78a6a2267864721155f7
Miguel Angel Zetina Zetina <loefthur26@gmail.com>
2018-04-24 16:11:54 -0500

    Using setUp method

--- stock_alerter/tests/test_stock.py
+++ stock_alerter/tests/test_stock.py
@@ -6,9 +6,12 @@ from ..stock import Stock
 
 
 class StockTest(unittest.TestCase):
+
+    def setUp(self):
+        self.goog = Stock("GOOG")
+
     def test_price_of_a_new_stock_class_shoulb_be_None(self):
-        stock = Stock("GOOG")
-        self.assertIsNone(stock.price)
+        self.assertIsNone(self.goog.price)
 
     def test_stock_update(self):
         """
@@ -16,17 +19,14 @@ class StockTest(unittest.TestCase):
         We will be using the 'datetime' module for the timestamp
         This test follows the pattern Arrange-Act-Assert
         """
-        goog = Stock("GOOG")
-        goog.update(datetime(2014, 2, 12), price=10)
-        self.assertEqual(10, goog.price)
+        self.goog.update(datetime(2014, 2, 12), price=10)
+        self.assertEqual(10, self.goog.price)
 
     def test_negative_price_should_throw_ValueError(self):
-        goog = Stock("GOOG")
         with self.assertRaises(ValueError):
-            goog.update(datetime(2014, 2, 13), -13)
+            self.goog.update(datetime(2014, 2, 13), -13)
 
     def test_stock_price_should_give_the_latest_price(self):
-        goog = Stock("GOOG")
-        goog.update(datetime(2014, 2, 12), price=10)
-        goog.update(datetime(2014, 2, 13), price=8.4)
-        self.assertAlmostEqual(8.4, goog.price, delta=0.0001)
+        self.goog.update(datetime(2014, 2, 12), price=10)
+        self.goog.update(datetime(2014, 2, 13), price=8.4)
+        self.assertAlmostEqual(8.4, self.goog.price, delta=0.0001)
